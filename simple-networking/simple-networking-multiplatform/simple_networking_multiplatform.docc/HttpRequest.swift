93695d665fd4df82006eb3fedc67021d26a412e9
Marko Randjelovic <marko.randjelovic@zuehlke.com>
2023-03-06 16:18:26 +0100

    request outline

---
+++ simple-networking/simple-networking-multiplatform/simple_networking_multiplatform.docc/HttpRequest.swift
@@ -0,0 +1,31 @@
+//
+//  HttpRequest.swift
+//  
+//
+//  Created by Marko Randjelovic on 6.3.23..
+//
+
+import Foundation
+
+
+enum HttpMethod {
+    case get, post, put, delete, patch
+}
+
+struct Request {
+    
+    func fetch(_ request: HttpRequest) -> Result<ResponseType, Error> {
+        let request = makeRequest(from: request)
+        URLSession.shared.data(for: request)
+    }
+    
+    func makeRequest(from request: HttpRequest) -> URLRequest {
+        URLRequest(url: URL(string: "www.google.com"))
+    }
+}
+
+
+protocol HttpRequest {
+    let path: String {get}
+    let method: HttpMethod {get}
+}
