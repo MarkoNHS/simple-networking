4efe95171946d7d9f07c73193ef530e9b0f27772
Michele Fadda <michele.fadda@zuhlke.com>
2023-03-06 14:27:20 +0000

    initial Xcode project setup
    starting as multiplatform prj

---
+++ simple-networking/simple-networking-multiplatform/simple_networking_multiplatform.h
@@ -0,0 +1,18 @@
+//
+//  simple_networking_multiplatform.h
+//  simple-networking-multiplatform
+//
+//  Created by Michele Fadda on 06/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for simple_networking_multiplatform.
+FOUNDATION_EXPORT double simple_networking_multiplatformVersionNumber;
+
+//! Project version string for simple_networking_multiplatform.
+FOUNDATION_EXPORT const unsigned char simple_networking_multiplatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <simple_networking_multiplatform/PublicHeader.h>
+
+
